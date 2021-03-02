3fc6264459e1962d393b6c29ce44ed5e8ee79480
prince chauhan <bolbam13@gmail.com>
2021-03-02 15:58:08 +0530

    selectionsort.c

---
+++ selectionsort.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+void swap(int *a,int *b){
+    int temp =*a;
+    *a = *b;
+    *b = temp;
+}
+void selection_sort(int arr[],int n){
+for(int i=0;i<n-1;i++){
+       int minindex=i;
+        for(int j=i+1;j<n;j++){
+            if(arr[j]<arr[minindex])
+                minindex=j;
+        }
+        swap(&arr[minindex],&arr[i]);
+    }  
+    }  
+void print(int arr[],int n){
+        for(int i=0;i<n;i++){
+        printf("%d ",arr[i]);
+        }
+    printf("\n");
+}
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    selection_sort(arr,n);
+    print(arr,n);
+return 0;
+}
