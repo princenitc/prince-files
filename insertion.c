c565cb97e7e1ccde9236909322321d30720c70a3
prince chauhan <bolbam13@gmail.com>
2021-03-03 10:23:04 +0530

    updated insertion sort algorithm

--- insertion.c
+++ insertion.c
@@ -8,7 +8,7 @@ void print(int arr[],int n){
         printf("\n");
 }
 void insertion_sort(int arr[],int n){
-        for(int i=0;i<n;i++){
+        for(int i=1;i<n;i++){
             int key =arr[i];
             int j=i-1;
             while(j >=0 && arr[j]>key){
